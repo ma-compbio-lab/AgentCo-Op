d7a0796b034bcdc12a5ea81cf20eacbf24cbf089
Shuaike Shen <shenshuaike256@gmail.com>
2026-03-03 00:19:42 -0500

    [feat] clean and reformat codebase

--- methods/__init__.py
+++
@@ -1,8 +0,0 @@
-from methods import adaptive, baseline, orchestrated_mas, sequential_mas
-
-METHODS = {
-    "adaptive": adaptive.run,
-    "baseline": baseline.run,
-    "sequential": sequential_mas.run,
-    "orchestrated": orchestrated_mas.run,
-}
